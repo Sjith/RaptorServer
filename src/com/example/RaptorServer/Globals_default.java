9f79d83f118c49b05499cc660b5192a32e7e2a9b
Hebron George <hebrontgeorge@gmail.com>
2012-12-31 18:15:41 -0500

    Local commit to clean up files.

--- src/com/example/RaptorServer/Globals_default.java
+++
@@ -1,9 +0,0 @@
-package com.example.RaptorServer;
-
-
-public class Globals_default {
-    public static final String hostname = "127.0.0.1";
-    public static final String username = "raptorserver";
-    public static final String password = "password";
-}
-
