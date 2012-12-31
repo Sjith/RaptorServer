c69a0eb02e506931ea3c3ff44e1843fc5d6ae11e
Robert Skinner <ras1@mail.usf.edu>
2012-12-31 06:40:35 -0500

    Got simple example performing "ls" command on remote server and printing to logcat.

--- src/com/example/RaptorServer/Globals_default.java
+++ src/com/example/RaptorServer/Globals_default.java
@@ -2,7 +2,7 @@ package com.example.RaptorServer;
 
 
 public class Globals_default {
-    public static final String hostname = "127.0.0.1";
+    public static final String hostname = "127.0.5.1";
     public static final String username = "raptorserver";
     public static final String password = "password";
 }
